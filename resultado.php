37692401cf6a8c86736d271b4dbe28e401549b4b
richmito <richelmypalmito@hotmail.com>
2017-07-21 20:58:49 -0300

    versão alfa do programa pra exibir o resultado

---
+++ resultado.php
@@ -0,0 +1,23 @@
+<html>
+<body>
+
+
+
+As respostas do seu teste:
+<?php
+//echo  nl2br ("kings \n garden");
+
+echo nl2br("\nresultado da questão 1: ") . $_POST["questao1"];
+echo nl2br("\nresultado da questão 2: ") . $_POST["questao2"] ;
+echo nl2br("\nresultado da questão 3: ") . $_POST["questao3"] ;
+echo nl2br("\nresultado da questão 4: ") . $_POST["questao4"] ;
+echo nl2br("\nresultado da questão 5: ") . $_POST["questao5"] ;
+echo nl2br("\nresultado da questão 6: ") . $_POST["questao6"] ;
+echo nl2br("\nresultado da questão 7: ") . $_POST["questao7"] ;
+echo nl2br("\nresultado da questão 8: ") . $_POST["questao8"] ;
+echo nl2br("\nresultado da questão 9: ") . $_POST["questao9"] ;
+echo nl2br("\nresultado da questão 10: ") . $_POST["questao10"];
+echo nl2br("\nresultado da questão 11: ") . $_POST["questao11"];
+?>
+</body>
+</html>
