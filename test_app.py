1891c61cc22680a3613087ec91cb0f894c9b74a1
Richelmy Monteiro <richelmy.monteiro@gmail.com>
2018-09-30 03:59:52 -0300

    Adicionados novos testes e template para mais alguns

--- test_app.py
+++ test_app.py
@@ -9,7 +9,17 @@ def cadastrar_pergunta(numero_in,texto_in):
     pergunta = Perguntas(numero=numero_in,texto=texto_in)
     pergunta.save()
 
+def ler_texto_pergunta(self):
+    pergunta = Perguntas.objects.get(numero=self)
+    return pergunta.texto
 
+def remover pergunta(self):
+    pergunta = Perguntas.objects.get(numero=self)
+    pergunta.delete()
+
+def redundancia(self):
+    # verifica se existe mais de um valor com o mesmo numero
+    valor = Perguntas.objects.filter(numero=self)
 
 class TestUsuarios(TestCase):
     def setUp(self):
@@ -24,8 +34,18 @@ class TestUsuarios(TestCase):
         tam_docu_novo = Perguntas.objects.count()
         self.assertGreater(tam_docu_novo,tam_docu,"O número de perguntas após o teste é maior que antes")
 
-    def tearDown(self):
+    def test_ler_texto_pergunta(self):
+        texto_pergunta = ler_texto_pergunta(1)
+        self.assertEquals(texto_pergunta,'Que tipo de lugar você acha que reflete mais a sua personalidade e te deixaria mais à vontade?',"o texto lido no banco é idêntico ao comparado no teste")
+
+    def test_deletar_pergunta(self):
+        remover_pergunta(5)
+        #verificar se o numero de perguntas permanece o mesmo
+
+
+    #def tearDown(self):
         #self.db.drop_database(os.environ['MONGODB_NAME'])
-        self.db.drop_database(MONGODB_NAME)
+        #self.db.drop_database(MONGODB_NAME)
+
 
 main()
