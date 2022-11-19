d34d78581a66bacfa5ec30bc4e8fc7c95521afdd
Richelmy Monteiro <richelmy.monteiro@gmail.com>
2022-11-18 23:05:43 -0300

    Opcoes de letra na resposta predefinida

--- proj_teste_sdb/app_teste_sdb/models/Questionario.py
+++ proj_teste_sdb/app_teste_sdb/models/Questionario.py
@@ -1,4 +1,11 @@
 from django.db import models
+from string import ascii_uppercase as letras
+
+LISTA_LETRAS = []
+for letra in letras:
+    LISTA_LETRAS.append((letra,letra),)
+
+LISTA_LETRAS = tuple(LISTA_LETRAS)
 
 class Questionario(models.Model):
     id_questionario = models.IntegerField(unique=True, blank=False, primary_key=True)
@@ -14,4 +21,4 @@ class Resposta(models.Model):
     questionario = models.ForeignKey(Questionario, on_delete=models.CASCADE)
     pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
     texto_resposta = models.CharField(max_length=255, blank=False)
-    letra_alternativa = models.CharField(max_length=2, blank=False)
+    letra_alternativa = models.CharField(max_length=2, blank=False, choices=LISTA_LETRAS, default='A')
