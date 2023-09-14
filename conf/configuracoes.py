238ea2e2d093fa00734ee3e28faeee66b99ef1b3
Richelmy Monteiro <richelmy.monteiro@gmail.com>
2023-09-14 20:00:23 -0300

    Base inicial do django

--- conf/configuracoes.py
+++ conf/configuracoes.py
@@ -5,4 +5,4 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-SECRET_KEY_ENV = os.environ.get("SECRET_KEY_ENV")
+SECRET_KEY_GENERATED = os.environ.get("SECRET_KEY_GENERATED")
--- proj_teste_sdb/app_teste_sdb/views/hello.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.contrib import messages
-import sys, os
-sys.path.append(os.getcwd())
-
-@csrf_exempt
-def hello(request):
-    return HttpResponse('Hello World')
